bada436cf8ac67b306308dade13ec7f100bead73
Benjamin Herzog <mail@benchr.de>
2017-08-18 11:12:27 +0200

    Added xcodeproj, Cocoapods and ParserCombinator via pods

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Benjamin Herzog on 18.08.17.
+//  Copyright © 2017 Benjamin Herzog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,14 @@
+platform :ios, '9.0'
+
+target 'Calculator' do
+  use_frameworks!
+
+  # Pods for Calculator
+  pod 'ParserCombinator', :git => 'https://github.com/BenchR267/ParserCombinator', :branch => 'master'
+
+  target 'CalculatorTests' do
+    inherit! :search_paths
+    
+  end
+
+end
